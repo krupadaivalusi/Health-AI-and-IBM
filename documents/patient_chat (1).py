2766b7614e124cba052ba1cbc6ded56d01a06dbe
krupa daiva lusi <jestadinaganna@gmail.com>
2025-08-03 10:39:50 +0530

    Delete documents directory

--- documents/patient_chat (1).py
+++
@@ -1,4 +0,0 @@
-
-def patient_chat_response(query):
-    # Mocked response
-    return f"This is a general answer to your question: '{query}'. Please consult a healthcare provider for accurate diagnosis."
